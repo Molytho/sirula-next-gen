e2998d76b318e39db8ca6be3cf0d33218b1787b7
Robin Ebert <ebertrobin2002@gmail.com>
2022-02-01 14:52:33 +0100

    Implement Dirs and Config

---
+++ src/config.rs
@@ -0,0 +1,134 @@
+use log::debug;
+
+use crate::Dirs;
+use crate::dirs::DirError;
+use core::fmt::Display;
+use std::borrow::Borrow;
+use std::error::Error;
+use std::io::Read;
+use std::fs::File;
+use std::path::Path;
+use toml::Value;
+use toml::value::Table;
+use serde::de::Deserialize;
+
+static DEFAULT_CONFIG: &str = "config.toml";
+
+#[derive(Debug)]
+pub enum ConfigErrorType {
+    IOError,
+    FormatError,
+    NotFound,
+    WrongType
+}
+
+#[derive(Debug)]
+pub struct ConfigError {
+    r#type: ConfigErrorType,
+    inner: Option<Box<dyn Error>>
+}
+impl Display for ConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        if let Some(e) = &self.inner {
+            let e: &(dyn Error) = e.borrow();
+            write!(f, "{:?} caused by {}", self.r#type, e)
+        } else {
+            write!(f, "{:?}", self.r#type)
+        }
+    }
+}
+impl Error for ConfigError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        if let Some(e) = &self.inner {
+            Some(e.borrow())
+        } else {
+            None
+        }
+    }
+}
+impl ConfigError {
+    fn new(r#type: ConfigErrorType, inner: Option<Box<dyn Error>>) -> ConfigError {
+        ConfigError { r#type, inner }
+    }
+    pub fn get_type(&self) -> &ConfigErrorType {
+        &self.r#type
+    }
+}
+impl From<std::io::Error> for ConfigError {
+    fn from(inner: std::io::Error) -> Self {
+        ConfigError::new(ConfigErrorType::IOError, Some(Box::new(inner)))
+    }
+}
+impl From<DirError> for ConfigError {
+    fn from(inner: DirError) -> Self {
+        ConfigError::new(ConfigErrorType::IOError, Some(Box::new(inner)))
+    }
+}
+impl From<toml::de::Error> for ConfigError {
+    fn from(inner: toml::de::Error) -> Self {
+        ConfigError::new(ConfigErrorType::FormatError, Some(Box::new(inner)))
+    }
+}
+impl From<ConfigErrorType> for ConfigError {
+    fn from(r#type: ConfigErrorType) -> Self {
+        ConfigError::new(r#type, None)
+    }
+}
+
+type ConfigResult<T> = Result<T, ConfigError>;
+
+#[derive(Debug)]
+pub struct Config {
+    config: Table
+}
+
+impl Config {
+    fn from_file(mut file: File) -> ConfigResult<Config> {
+        debug!("Opening config file: {:?}", file);
+        let mut config_toml = String::new();
+        file.read_to_string(&mut config_toml)?;
+
+        let table = config_toml.parse::<Value>()?;
+        debug_assert!(table.is_table());
+        Ok(Config { config: table.try_into()? })
+    }
+
+    pub fn new(dirs: &Dirs) -> ConfigResult<Config> {
+        let config_file = dirs.get_config_file(DEFAULT_CONFIG)?;
+
+        Config::from_file(config_file)
+    }
+
+    pub fn from_path(path: &Path) -> ConfigResult<Config> {
+        Config::from_file(File::open(path)?)
+    }
+
+    pub fn get_module_config<'a>(&'a self, name: &str) -> ConfigResult<ModuleConfig<'a>> {
+        if self.config.contains_key(name) {
+            let value = &self.config[name];
+            if value.is_table() {
+                Ok(ModuleConfig { values: value.as_table().expect("Toml library broke assumption") })
+            } else {
+                Err(ConfigErrorType::WrongType.into())
+            }
+        } else {
+            Err(ConfigErrorType::NotFound.into())
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ModuleConfig<'a> {
+    values: &'a Table
+}
+
+impl <'a> ModuleConfig<'a> {
+    pub fn get_config<'de, T : Deserialize<'de>>(&self, name: &str) -> ConfigResult<T> {
+        if self.values.contains_key(name) {
+            self.values[name].clone().try_into()
+                .or(Err(ConfigErrorType::WrongType.into()))
+        } else {
+            Err(ConfigErrorType::NotFound.into())
+        }
+    }
+}
---
+++ src/dirs.rs
@@ -0,0 +1,76 @@
+use log::debug;
+
+use core::fmt::Display;
+use std::error::Error;
+use std::fs::{File, OpenOptions};
+use xdg::BaseDirectoriesError;
+
+#[derive(Debug)]
+pub enum DirError {
+    DirNotAccesible(BaseDirectoriesError),
+    FileNotFound(std::io::Error),
+}
+impl From<std::io::Error> for DirError {
+    fn from(inner: std::io::Error) -> Self {
+        DirError::FileNotFound(inner)
+    }
+}
+impl From<BaseDirectoriesError> for DirError {
+    fn from(inner: BaseDirectoriesError) -> Self {
+        DirError::DirNotAccesible(inner)
+    }
+}
+impl Display for DirError {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        match self {
+            DirError::DirNotAccesible(e) => {
+                write!(fmt, "DirNotAccesible caused by {}", e)
+            },
+            DirError::FileNotFound(e) => {
+                write!(fmt, "FileNotFounde caused by {}", e)
+            }
+        }
+    }
+}
+impl Error for DirError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            DirError::FileNotFound(e) => {
+                Some(e)
+            }
+            DirError::DirNotAccesible(e) => {
+                Some(e)
+            }
+        }
+    }
+}
+
+type DirResult<T> = Result<T, DirError>;
+
+#[derive(Debug)]
+pub struct Dirs {
+    xdg_dirs: xdg::BaseDirectories
+}
+
+impl Dirs {
+    pub fn new(dir_name: &str) -> DirResult<Dirs> {
+        debug!("Creating Dirs object with {}", dir_name);
+        Ok(Dirs { xdg_dirs: xdg::BaseDirectories::with_prefix(dir_name)? })
+    }
+
+    pub fn get_config_file(&self, name: &str) -> DirResult<File> {
+        debug!("Try opening config file with name {}", name);
+        let path = self.xdg_dirs.get_config_file(name);
+        Ok(File::open(path)?)
+    }
+
+    pub fn get_cache_file(&self, name: &str) -> DirResult<File> {
+        debug!("Opening cache file with name {}", name);
+        let path = self.xdg_dirs.place_cache_file(name)?;
+        Ok(OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path)?)
+    }
+}
